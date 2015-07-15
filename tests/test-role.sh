a32bc5e3604701104e1fc4e692c6c76c5689fed4
Barney Hanlon <bhanlon@inviqa.com>
2015-07-15 07:13:13 +0100

    More test data and change ansible version

--- tests/test-role.sh
+++ tests/test-role.sh
@@ -4,7 +4,7 @@ test_role() {
   local ANSIBLE_
   local ANSIBLE_INSTALLER_URI='https://raw.githubusercontent.com/shrikeh/ansible-virtualenv/master/init.sh';
   local ANSIBLE_VENV='.venv';
-  local ANSIBLE_VERSION='devel';
+  local ANSIBLE_VERSION='v1.9.2-1';
   local ANSIBLE_CHECKOUT_PATH='./.ansible';
   local ANSIBLE_ROLES_FILE='./requirements.yml';
   local ANSIBLE_INVENTORY_FILE='./inventory';
@@ -13,8 +13,9 @@ test_role() {
 
   . <(curl -L --silent "${ANSIBLE_INSTALLER_URI}") \
     -d "${ANSIBLE_CHECKOUT_PATH}" \
-    --venv "${ANSIBLE_VENV}" \
     --branch "${ANSIBLE_VERSION}" \
+    --venv "${ANSIBLE_VENV}" \
+    --pip-version
   ;
   mkdir -p "${ANSIBLE_ROLES_LOCAL_PATH}";
 
@@ -25,7 +26,8 @@ test_role() {
 
   ( export ANSIBLE_ROLES_PATH=${ANSIBLE_ROLES_LOCAL_PATH}; \
     ansible-playbook -i "${ANSIBLE_INVENTORY_FILE}" \
-    "${ANSIBLE_PLAYBOOK}"
+    "${ANSIBLE_PLAYBOOK}" \
+    --ask-vault-pass \
   );
 }
 
